9ef650803f7003a5dd419205fc3bdd7ec08515a0
DianaCato <diana.catolico@hotmail.com>
2021-06-11 19:53:51 -0500

    only google map

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import MapScreen from './src/screens/MapScreen'
+import MapListScreen from './src/screens/MapListScreen'
+import { setNavigator } from './src/navigationRef'
 
-export default function App() {
+const switchNavigator = createSwitchNavigator({
+  mainFlow: createBottomTabNavigator({
+    MapView: MapScreen,
+    MapList: MapListScreen
+  })
+})
+
+const App = createAppContainer(switchNavigator)
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <App 
+      ref={navigator => {
+        setNavigator(navigator)
+      }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
